4f8d7b419deab83fb95662911f0d4ae6646ab5e2
AKrekhovetskyi <akrechovetskiy@gmail.com>
2025-07-10 20:04:02 +0300

    refactor: reuse 'CreateMixin'

--- livemap/serializers.py
+++ livemap/serializers.py
@@ -19,21 +19,13 @@ class CreateMixin:
         return self.Meta.model.objects.create(**validated_data)
 
 
-class VideoStreamSourceSerializer(serializers.ModelSerializer):
+class VideoStreamSourceSerializer(CreateMixin, serializers.ModelSerializer):
     parking_lot_address = serializers.CharField(source="parking_lot.address", read_only=True)
     parking_lot_id = serializers.IntegerField(help_text="ID of an existing parking lot.")
 
-    class Meta:
+    class Meta:  # pyright: ignore[reportIncompatibleVariableOverride]
         model = VideoStreamSource
         fields = ("id", "parking_lot_address", "parking_lot_id", "stream_source", "processing_rate", "is_active")
-
-    def create(self, validated_data: dict[str, Any]) -> VideoStreamSource:
-        parking_lot_id = validated_data.get("parking_lot_id")
-        if parking_lot_id and not ParkingLot.objects.filter(id=parking_lot_id).first():
-            raise serializers.ValidationError(
-                {"parking_lot_id": f"Parking lot with the {parking_lot_id} ID not found."}
-            )
-        return VideoStreamSource.objects.create(**validated_data)
 
 
 class OccupancySerializer(CreateMixin, serializers.ModelSerializer):
