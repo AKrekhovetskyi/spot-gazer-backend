d2aff2dc1e6e895188d8be5597cd06c8531b17b2
AKrekhovetskyi <akrechovetskiy@gmail.com>
2025-07-10 17:50:29 +0300

    refactor: group streams

--- livemap/drf_views.py
+++ livemap/drf_views.py
@@ -1,7 +1,8 @@
-from typing import ClassVar
+from typing import Any, ClassVar
 
 from django.db.models import QuerySet
 from rest_framework import permissions, viewsets
+from rest_framework.response import Response
 
 from .models import VideoStreamSource
 from .serializers import VideoStreamSourceSerializer
@@ -19,3 +20,39 @@ class VideoStreamSourceViewSet(viewsets.ModelViewSet):
         if active_only:
             return self.queryset.filter(is_active=True)
         return self.queryset
+
+    def list(self, *args: Any, **kwargs: Any) -> dict[str, Any]:  # noqa: ARG002
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        video_streams = serializer.data
+        stream_details = {}
+        for stream in video_streams:
+            if stream["parking_lot_id"] in stream_details:
+                stream_details[stream["parking_lot_id"]].append(
+                    {"id": stream["id"], "stream_source": stream["stream_source"], "is_active": stream["is_active"]}
+                )
+            else:
+                stream_details[stream["parking_lot_id"]] = [
+                    {
+                        "id": stream["id"],
+                        "stream_source": stream["stream_source"],
+                        "is_active": stream["is_active"],
+                    }
+                ]
+
+        grouped_video_streams = []
+        for video_stream in video_streams:
+            try:
+                grouped_video_streams.append(
+                    {
+                        "parking_lot": video_stream["parking_lot"],
+                        "parking_lot_id": video_stream["parking_lot_id"],
+                        "processing_rate": video_stream["processing_rate"],
+                        "streams": stream_details.pop(video_stream["parking_lot_id"]),
+                    }
+                )
+            except KeyError:
+                # The `KeyError` is expected because there are might be duplicates in the `video_streams`.
+                continue
+
+        return Response(grouped_video_streams)
