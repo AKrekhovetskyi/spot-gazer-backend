c5df43741500163717157ca0d68caf2c5ce528a9
AKrekhovetskyi <akrechovetskiy@gmail.com>
2025-07-10 15:55:00 +0300

    test: remove 'TestCase'; register user
    Replaced hard-coded "stream_source" with fake ones.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,24 @@
-from django.test import TestCase
+from typing import TYPE_CHECKING
+
+from django.contrib.auth import get_user_model
 from faker import Faker
 
 from livemap.models import Address, City, Country, ParkingLot, VideoStreamSource
 
+if TYPE_CHECKING:
+    from django.contrib.auth.models import UserManager
+
 fake = Faker()
 
 
-class TestCaseWithData(TestCase):
+class TestCaseWithData:
     def setUp(self) -> None:
         # Set up data for the whole TestCase
+        user_manager: UserManager = get_user_model().objects  # type: ignore[reportAssignmentType]
+        self.username = fake.pystr()
+        self.password = fake.pystr()
+        self.user = user_manager.create_user(username=self.username, password=self.password)
+
         self.country = Country.objects.create(country_name=fake.country())
         self.city = City.objects.create(country=self.country, city_name=fake.city())
         self.address = Address.objects.create(city=self.city, parking_lot_address=fake.street_address())
@@ -22,14 +32,14 @@ class TestCaseWithData(TestCase):
         )
         self.stream_source_data = {
             "parking_lot": self.parking_lot,
-            "stream_source": "tests/test_media/small_parking.jpg",
+            "stream_source": fake.url(),
             "processing_rate": fake.pyint(min_value=1, max_value=5),
             "is_active": True,
         }
         self.small_parking_lot = VideoStreamSource.objects.create(**self.stream_source_data)
         self.big_parking_lot = VideoStreamSource.objects.create(
             parking_lot=self.parking_lot,
-            stream_source="tests/test_media/big_parking.jpg",
+            stream_source=fake.url(),
             processing_rate=self.small_parking_lot.processing_rate,
             is_active=True,
         )
