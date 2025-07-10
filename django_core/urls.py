2896474b39b499550aa1e2dc2a533d0f33df1b89
AKrekhovetskyi <akrechovetskiy@gmail.com>
2025-07-10 09:18:37 +0300

    feat: add 'api-auth' endpoint

--- django_core/urls.py
+++ django_core/urls.py
@@ -22,5 +22,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("livemap.urls", namespace="livemap")),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
