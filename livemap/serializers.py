72977db307c2cacd6e86a8edc02375b7b85a0b5a
AKrekhovetskyi <akrechovetskiy@gmail.com>
2025-07-10 20:00:42 +0300

    feat: add 'Occupancy' serializer

--- livemap/serializers.py
+++ livemap/serializers.py
@@ -1,8 +1,22 @@
 from typing import Any
 
+from django.db.models import Model
 from rest_framework import serializers
 
-from .models import ParkingLot, VideoStreamSource
+from .models import Occupancy, ParkingLot, VideoStreamSource
+
+
+class CreateMixin:
+    class Meta:
+        model: Model
+
+    def create(self, validated_data: dict[str, Any]) -> Model:
+        parking_lot_id = validated_data.get("parking_lot_id")
+        if parking_lot_id and not ParkingLot.objects.filter(id=parking_lot_id).first():
+            raise serializers.ValidationError(
+                {"parking_lot_id": f"Parking lot with the {parking_lot_id} ID not found."}
+            )
+        return self.Meta.model.objects.create(**validated_data)
 
 
 class VideoStreamSourceSerializer(serializers.ModelSerializer):
@@ -20,3 +34,12 @@ class VideoStreamSourceSerializer(serializers.ModelSerializer):
                 {"parking_lot_id": f"Parking lot with the {parking_lot_id} ID not found."}
             )
         return VideoStreamSource.objects.create(**validated_data)
+
+
+class OccupancySerializer(CreateMixin, serializers.ModelSerializer):
+    parking_lot_id = serializers.IntegerField(help_text="ID of an existing parking lot.")
+    timestamp = serializers.DateTimeField(read_only=True)
+
+    class Meta:  # pyright: ignore[reportIncompatibleVariableOverride]
+        model = Occupancy
+        fields = ("id", "parking_lot_id", "occupied_spots", "timestamp")
