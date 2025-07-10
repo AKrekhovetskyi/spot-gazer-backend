6b4b179597c8bd194e301f80c6b3443c43e68cb6
AKrekhovetskyi <akrechovetskiy@gmail.com>
2025-07-10 09:18:57 +0300

    feat: add 'api' endpoint

--- livemap/urls.py
+++ livemap/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
 from livemap.views import index
 
+router = routers.DefaultRouter()
 urlpatterns = [
     path("", index, name="index"),
+    path("api/", include(router.urls)),
 ]
 
 app_name = "livemap"
