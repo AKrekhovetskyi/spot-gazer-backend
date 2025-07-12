edf1602d3f99650e6642d2ba4ca050a648e3e224
AKrekhovetskyi <akrechovetskiy@gmail.com>
2025-07-12 17:14:24 +0300

    test: 'mark_in_use_until' filter
    - select predefined 'processing_rate';
    - use 'query_params'.

--- tests/livemap/test_api_endpoints.py
+++ tests/livemap/test_api_endpoints.py
@@ -1,4 +1,6 @@
 from copy import deepcopy
+from datetime import UTC, datetime, timedelta
+from time import sleep
 
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -29,7 +31,7 @@ class VideoStreamSourceTests(TestCaseWithData, APITestCase):
         }
         self.client.login(username=self.username, password=self.password)
         request = self.client.post(self.video_stream_path, content_type=CONTENT_TYPE, data=fake_stream)
-        self.assertEqual(request.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(request.status_code, status.HTTP_201_CREATED, msg=request.json())
         self.assertEqual(request.json()["stream_source"], fake_stream["stream_source"])
 
         fake_stream_copy = deepcopy(fake_stream)
@@ -42,8 +44,9 @@ class VideoStreamSourceTests(TestCaseWithData, APITestCase):
         )
 
         fake_stream_copy = deepcopy(fake_stream)
-        fake_processing_rate = fake.pyint(min_value=6)
-        fake_stream_copy["processing_rate"] = fake_processing_rate
+        processing_rate = VideoStreamSource.ProcessingRate.values
+        processing_rate.remove(self.stream_source_data["processing_rate"])
+        fake_stream_copy["processing_rate"] = fake.random_element(processing_rate)
         request = self.client.post(self.video_stream_path, content_type=CONTENT_TYPE, data=fake_stream_copy)
         self.assertEqual(request.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("The processing rate for the parking lot", request.json()["processing_rate"][0])
@@ -59,10 +62,12 @@ class VideoStreamSourceTests(TestCaseWithData, APITestCase):
         response = self.client.get(f"{self.video_stream_path}1/", content_type=CONTENT_TYPE)
         self.assertEqual(response.json()["is_active"], False)
 
+        processing_rate = VideoStreamSource.ProcessingRate.values
+        processing_rate.remove(self.stream_source_data["processing_rate"])
         response = self.client.patch(
             f"{self.video_stream_path}1/",
             content_type=CONTENT_TYPE,
-            data={"id": 1, "processing_rate": self.stream_source_data["processing_rate"] + 1},
+            data={"id": 1, "processing_rate": fake.random_element(processing_rate)},
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
@@ -70,13 +75,44 @@ class VideoStreamSourceTests(TestCaseWithData, APITestCase):
             "`parking_lot_id` field must be provided along with the `processing_rate` one",
         )
 
-    def test_filters(self) -> None:
+    def test_active_only_filter(self) -> None:
         response = self.client.get(self.video_stream_path, content_type=CONTENT_TYPE)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         VideoStreamSource.objects.filter(pk=self.big_parking_lot.pk).update(is_active=False)
-        filtered_response = self.client.get(f"{self.video_stream_path}?active_only=true", content_type=CONTENT_TYPE)
+        filtered_response = self.client.get(
+            self.video_stream_path, content_type=CONTENT_TYPE, query_params={"active_only": True}
+        )
         self.assertEqual(len(response.json()[0]["streams"]) - 1, len(filtered_response.json()))
+
+    def test_mark_in_use_until_filter(self) -> None:
+        delta_seconds = timedelta(seconds=2)
+        in_use_until = (datetime.now(UTC) + delta_seconds).isoformat()
+        response = self.client.get(
+            f"{self.video_stream_path}", content_type=CONTENT_TYPE, query_params={"mark_in_use_until": in_use_until}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.json())
+
+        # All returned streams should have `in_use_until` set to the requested value
+        for group in response.json():
+            for stream in group["streams"]:
+                self.assertEqual(datetime.fromisoformat(stream["in_use_until"]), datetime.fromisoformat(in_use_until))
+
+        # All streams are in use. Nothing is returned.
+        in_use_until = (datetime.now(UTC) + delta_seconds).isoformat()
+        response = self.client.get(
+            f"{self.video_stream_path}", content_type=CONTENT_TYPE, query_params={"mark_in_use_until": in_use_until}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.json())
+        self.assertEqual(response.json(), [])
+
+        sleep(delta_seconds.seconds)
+        in_use_until = (datetime.now(UTC) + delta_seconds).isoformat()
+        response = self.client.get(
+            f"{self.video_stream_path}", content_type=CONTENT_TYPE, query_params={"mark_in_use_until": in_use_until}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.json())
+        self.assertNotEqual(response.json(), [])
 
 
 class OccupancyTests(TestCaseWithData, APITestCase):
