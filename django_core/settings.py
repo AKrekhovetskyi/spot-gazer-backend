0caec58880627bf4e156eb377d3eb5da0e3d2130
AKrekhovetskyi <akrechovetskiy@gmail.com>
2025-07-15 14:50:07 +0300

    feat: register JWT auth

--- django_core/settings.py
+++ django_core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "livemap",
     "debug_toolbar",
     "rest_framework",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 50,
+    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
 }
 
 # Internationalization
