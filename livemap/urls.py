2f71439eee2a0f4446ad3a449845a7d3eb7aef2b
AKrekhovetskyi <akrechovetskiy@gmail.com>
2025-07-25 15:11:54 +0300

    refactor: 'views' -> 'view_sets'

--- livemap/urls.py
+++ livemap/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from livemap.drf.views import OccupancyViewSet, VideoStreamSourceViewSet
+from livemap.drf.view_sets import OccupancyViewSet, VideoStreamSourceViewSet
 from livemap.views import index
 
 router = routers.DefaultRouter()
